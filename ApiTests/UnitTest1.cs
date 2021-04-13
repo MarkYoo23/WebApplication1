b0e9746cbfa256cf361c49117494332a0dcb0aae
rd-dhyoo <dhyoo@rnd.re.kr>
2021-04-13 19:25:53 +0900

    Initial commit

---
+++ ApiTests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using WebApplication1.Controllers;
+using Xunit;
+
+namespace ApiTests
+{
+    public class UnitTest1
+    {
+
+        private readonly WeatherForecastController _weatherForecastController;
+
+        public UnitTest1()
+        {
+            Mock<ILogger<WeatherForecastController>> logger = new Mock<ILogger<WeatherForecastController>>();
+            _weatherForecastController = new WeatherForecastController(logger.Object);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var weatherForecasts = _weatherForecastController.Get();
+            Assert.NotEmpty(weatherForecasts);
+        }
+    }
+}
