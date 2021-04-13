71e57360941f26c14571986f69a6f969c3595184
rd-dhyoo <dhyoo@rnd.re.kr>
2021-04-13 20:12:04 +0900

    Fixed

--- ApiTests/UnitTest1.cs
+++ ApiTests/UnitTest1.cs
@@ -35,7 +35,7 @@ namespace ApiTests
         public void FailTest1()
         {
             var weatherForecasts = _weatherForecastController.Get();
-            Assert.Empty(weatherForecasts);
+            Assert.NotEmpty(weatherForecasts);
         }
     }
 }
